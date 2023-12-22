54d20feb38a155105a73143d55522cc8006f8a07
agusjoffre <aguslj16@gmail.com>
2023-12-22 14:38:14 -0300

    feat: handle change -> bill input

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,12 +1,20 @@
 import { Label,TextInput } from "keep-react";
 
-export function Bill() {
+export function Bill({ setBill, calculate }) {
+  
+  const handleChange = (e) => {
+    const bill = parseFloat(e.target.value)
+    setBill(bill)
+    calculate()
+  }
+  
     return (
     <div>
       <Label htmlFor="bill-input" value="Bill" />
       <TextInput
         id="bill-input"
-        color="gray"
+          color="gray"
+          onChange={handleChange}
       />
     </div>
     )
